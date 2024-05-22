15874b431a63b3e869c823e9c7df4578328525e5
Antonio Santana <asantana35@usf.edu>
2024-05-22 19:41:46 -0400

    cc_1.js
    "Create and test calcTip function."

--- cc_1.js
+++ cc_1.js
@@ -8,3 +8,11 @@
  // Output Details
  console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill+tip}`);
 
+ // Create Function for calcTip
+ function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+ }
+// Testing the function with $100
+console.log(calcTip(100)); // Expected output:20
+
+
